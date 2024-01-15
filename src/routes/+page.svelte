002e619dcb82d2d07524fa5abe1fb77d3ce550d2
Luis Romero <luis.romero@vt.atlascopco.com>
2024-01-15 21:34:21 +0100

    feat(client): Add basic styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,17 +41,92 @@
     }
 </script>
 
-<strong>{noun.word}</strong>
+<main>
+    <div class="noun">
+        <strong class:correct={hasAnswered && isMatch} class:incorrect={hasAnswered && !isMatch}>
+          {noun.word}
+        </strong>
+    </div>
 
-<button on:click={ () => matchArticle('die')}>die</button>
-<button on:click={ () => matchArticle('der')}>der</button>
-<button on:click={ () => matchArticle('das')}>das</button>
-
-{#if hasAnswered}
-    <strong>{isMatch ? 'Correct!' : 'Incorrect!'}</strong>
-    
-    {#if isMatch}
-        <button on:click={selectNewNoun}>Next</button>
+    <div>
+        <button on:click={() => matchArticle('die')}>die</button>
+        <button on:click={() => matchArticle('der')}>der</button>
+        <button on:click={() => matchArticle('das')}>das</button>
+    </div>
+  
+  
+    {#if hasAnswered}
+      <strong class:correct={isMatch} class:incorrect={!isMatch}>
+        {isMatch ? 'Correct!' : 'Incorrect!'}
+      </strong>
+  
+      {#if isMatch}
+        <button class="next" on:click={selectNewNoun}>Next</button>
+      {/if}
     {/if}
+</main>
 
-{/if}
+
+<style lang="scss">
+
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        margin: 0;
+        font-family: 'Arial', sans-serif;
+        background-color: #f5f5f5;
+        text-align: center;
+    }
+
+    div {
+        display: block;
+        // margin-bottom: ;
+    }
+  
+    strong {
+      font-size: 1.5rem;
+      margin-bottom: 1rem;
+      color: #333;
+    }
+  
+    button {
+      font-size: 1rem;
+      padding: 0.5rem 1rem;
+      margin: 0.5rem;
+      cursor: pointer;
+      border: 1px solid #ccc;
+      background-color: #fff;
+      color: #333;
+      border-radius: 4px;
+      transition: background-color 0.3s, color 0.3s;
+    }
+  
+    button:hover {
+      background-color: #333;
+      color: #fff;
+    }
+  
+    button:active {
+      transform: translateY(1px);
+    }
+  
+    strong.correct {
+      color: #4caf50;
+    }
+  
+    strong.incorrect {
+      color: #f44336;
+    }
+  
+    button.next {
+      background-color: #2196f3;
+      color: #fff;
+    }
+  
+    button.next:hover {
+      background-color: #0d47a1;
+    }
+</style>
+  
