34cfd0017cdf8b63f83c489c04040f46f14e2067
Luis Romero <luis.romero@atlascopco.com>
2024-09-13 11:59:12 +0200

    refactor(client): Add space.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,7 @@
           await fetchScores();
         }
       } catch (err) {
-          console.error("There was an error saving your information", err);
+          console.error("There was an error saving your information ", err);
       }
   }
 
